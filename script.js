036cef524512c85152a9a4b397d8074e4414ed6b
Aaron McCollum <mccollumaarons@gmail.com>
2021-09-21 19:06:41 +0900

    added gamePlay function

--- script.js
+++ script.js
@@ -13,3 +13,39 @@ let computerPlay = () => {
         return "Sissors";
     }
 }
+
+// Function representing a complete round of Rock, Paper, Sissors
+let gamePlay = (playerSelection, computerPlay) => {
+    // Player plays "Rock"
+    if (playerSelection === "rock") {
+        if (computerPlay === "paper") {
+            return "You lose! Paper beats Rock!";
+        } else if (computerPlay === "rock") {
+            return "It's a tie, try again!";
+        } else {
+            return "You win! Rock beats Sissors!";
+        }
+    }
+
+    // Player plays "Paper"
+    if (playerSelection === "paper") {
+        if (computerPlay === "sissors") {
+            return "You lose! Sissors beats Paper!";
+        } else if (computerPlay === "paper") {
+            return "It's a tie, try again!";
+        } else {
+            return "You win! Paper beats Rock!";
+        }
+    }
+
+    // Player plays "Sissors"
+    if (playerSelection === "sissors") {
+        if (computerPlay === "rock") {
+            return "You lose! Rock beats Sissors!";
+        } else if (computerPlay === "sissors") {
+            return "It's a tie, try again!";
+        } else {
+            return "You win! Sissors beats Paper!";
+        }
+    }
+}
